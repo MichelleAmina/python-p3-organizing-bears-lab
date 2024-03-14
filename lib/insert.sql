cf28937ed0de1fdc81a994765d338b33f5683917
Michelle Amina Okumu <michelleaokumu@gmail.com>
2024-03-14 09:17:41 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4 , 'M', 'brown', 'good', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 8 , 'M', 'black', 'poor', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4 , 'F', 'white', 'good', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 10 , 'M', 'brown', 'fair', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5 , 'F', 'black', 'poor', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 12 , 'M', 'brown', 'poor', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 2 , 'F', 'white', 'good', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8 , 'M', 'brown', 'fair', 1);
